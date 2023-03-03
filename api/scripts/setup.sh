fb001b30d7f5d6221de2aade26c28298af3c7e6f
mbml84 <boumax84@gmail.com>
2023-03-02 21:04:17 -0500

    chore:Update setup script

--- api/scripts/setup.sh
+++ api/scripts/setup.sh
@@ -1,7 +1,7 @@
 echo "Setup project..."
 
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+python "$SCRIPT_DIR"/manage.py collectstatic
 python "$SCRIPT_DIR"/manage.py migrate
 python "$SCRIPT_DIR"/manage.py createsuperuser
