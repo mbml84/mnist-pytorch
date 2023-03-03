c6b6db8053aa0745dba48976a5c168c1288f3547
mbml84 <boumax84@gmail.com>
2023-03-03 12:23:19 -0500

    chore:Set logging + Deployment setup (WIP)

--- api/src/core/prediction.py
+++ api/src/core/prediction.py
@@ -1,24 +1,46 @@
 from __future__ import annotations
 
+import logging
+
 import numpy as np
 import torch
 from PIL import Image
 
 from api import settings
 
-_MODEL = torch.jit.load(settings.WEIGHTS_PATH)
+__LOGGER__ = logging.getLogger(__name__)
+
+
+def _get_model():
+    try:
+        return torch.jit.load(settings.WEIGHTS_PATH)
+    except Exception:
+        __LOGGER__.exception(
+            'No model has been loaded. Prediction will always be -1',
+        )
+        return None
+
+
+_MODEL = _get_model()
 INPUT_SIZE = (28, 28)
 
 
-def predict(image_stream):
-    image = Image.open(image_stream).convert('L')
-    image = image.resize(INPUT_SIZE)
-    image_array = np.array(image).astype(np.float64) / 255
-    tensor_input = torch.tensor(image_array, dtype=torch.float).permute(
-        0, 1,
-    ).unsqueeze(0).unsqueeze(0)
-    prediction = _MODEL(tensor_input)
-    return torch.argmax(prediction).item() + 1
+def predict(image_stream) -> int:
+    if _MODEL:
+        image = Image.open(image_stream).convert('L')
+        image = image.resize(INPUT_SIZE)
+        image_array = np.array(image).astype(np.float64) / 255
+        tensor_input = torch.tensor(image_array, dtype=torch.float).permute(
+            0, 1,
+        ).unsqueeze(0).unsqueeze(0)
+        prediction = _MODEL(tensor_input)
+        prediction = torch.argmax(prediction).item() + 1
+    else:
+        __LOGGER__.warning(
+            'No model has been loaded. Prediction will always be -1',
+        )
+        prediction = -1
+    return prediction
 
 
 __all__ = [
